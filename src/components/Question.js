9929866786fb38056f7c28566115a04b2dda806e
Oscar-Otee <oluoch.oscar.oo@gmail.com>
2022-07-15 23:33:14 +0300

    commit tree 8d1dc85eb108c645196cd3f5f262d1b995bcfe56

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,9 +1,30 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
   // add useEffect code
+
+  useEffect(() => {
+    if (timeRemaining === 0) {
+      setTimeRemaining(10);
+      onAnswered(false);
+      return; 
+    }
+
+    
+    const timerId = setTimeout(() => {
+      
+      setTimeRemaining((timeRemaining) => timeRemaining - 1);
+    }, 1000);
+
+  
+    return function () {
+      clearTimeout(timerId);
+    };
+  }, [timeRemaining, onAnswered]);
+  
+
 
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
@@ -30,3 +51,4 @@ function Question({ question, onAnswered }) {
 }
 
 export default Question;
+
